85d29ff03960fc6fc5a6d5ac7fd6eba5baf7ee87
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-03-21 21:38:58 -0500

    feat: :lipstick: Haciendo las img dinamicas
    Haciendo las img dinamicas

---
+++ Js/app.js
@@ -0,0 +1,30 @@
+const Casas = [
+    {
+        "id": "Casa1",
+        "about": "Casa con 3 habitaciones, 2 baños, cocina integral, terraza con excelente vista hacia las montañas. Cercano a colegios, parques, hospitales, centros comerciales y con fácil acceso a transporte público. 🚗",
+        "picture": "https://pics.nuroa.com/alquilo_amoblado_irawa_ca%C3%B1averal_floridablanca_5360010685965752860.jpg",
+    "squarePic": "https://img-co-1.trovit.com/x1k1MT1EF1L1D/x1k1MT1EF1L1D.1_11.jpg",
+        "name": "Casa1"
+    },
+    {
+        "id": "Casa2",
+        "about": "",
+        "picture": "",
+    "squarePic": "",
+        "name": "Casa2"
+    },
+    {
+        "id": "Casa3",
+        "about": "",
+        "picture": "",
+    "squarePic": "",
+        "name": "Casa3"
+    },
+    {
+        "id": "Casa4",
+        "about": "",
+        "picture": "",
+    "squarePic": "",
+        "name": "Casa4"
+    }
+]
